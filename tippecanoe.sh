e49d248583a6a6ee4832465ce1a1178f6eafef6c
Michael Kreil <github@michael-kreil.de>
2024-06-06 17:57:13 +0200

    Update tippecanoe.sh

--- tippecanoe.sh
+++ tippecanoe.sh
@@ -1,8 +1,4 @@
-git clone --branch 1.35.0 https://github.com/mapbox/tippecanoe.git
-
+git clone https://github.com/felt/tippecanoe.git
 cd tippecanoe
-
 make -j
 make install
-
-tippecanoe --version
